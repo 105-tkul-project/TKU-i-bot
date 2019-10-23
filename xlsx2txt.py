96d5c6d67e5b0e6d3dedef8d378f2220c32e5e1e
Cheng Chia Shih <yugor313a@gmail.com>
2019-10-23 16:16:20 +0800

    add xml2txt

---
+++ xlsx2txt.py
@@ -0,0 +1,28 @@
+import xlrd
+import os
+# 要打開的語料庫
+wb = xlrd.open_workbook ('xml.xlsx')
+# 要存入的路徑
+base = os.getcwd ()+"\\text-classification-cnn-rnn\data"
+
+os.chdir (base)
+
+# 打開每一個工作表(sheet)
+for sheets in wb.sheet_names ():
+    # create directories
+    try:
+        os.mkdir (sheets)
+        # get in directory
+        os.chdir (base + f"/{sheets}")
+    except FileExistsError:
+        os.chdir (base + f"/{sheets}")
+
+    # get into sheet
+    ws = wb.sheet_by_name (sheets)
+    # iter rows
+    for i in range (ws.nrows):
+        # create txt with rows, overwrite if file exist
+        with open (f"{i}.txt", 'w') as f:
+            f.write (f"{ws.cell (i, 0).value}?{ws.cell (i, 1).value}")
+
+    os.chdir (base)
