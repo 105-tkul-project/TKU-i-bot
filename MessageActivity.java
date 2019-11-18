33c753c596cff62cc30397bf0c562cf8e586a712
Cheng Chia Shih <yugor313a@gmail.com>
2019-11-18 10:47:49 +0800

    final change

--- MessageActivity.java
+++ MessageActivity.java
@@ -4,73 +4,132 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 
-public class MessageActivity extends AppCompatActivity {
+public class MessageActivity extends AppCompatActivity implements View.OnClickListener {
     // 使用者發訊息
     private static final int VIEW_TYPE_MESSAGE_SENT = 0;
     // chat bot回訊息
     private static final int VIEW_TYPE_MESSAGE_RECEIVED = 1;
-
+    // object initailization
+    Button btn_back;
     ImageButton btn_send;
     EditText text_send;
     RecyclerView recyclerView;
     ArrayList<Chat> mchat = new ArrayList<>();
+    Intent intent;
+    MessageAdapter messageAdapter;
+    final Object syncObj = this; // Main thread
+    // Socket variable
+    private String ip = "163.13.28.46", msg_recv; // see shell>ipconfig
+    private int port = 8000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 綁定UI
+        btn_back = findViewById(R.id.btn_chat_back);
         btn_send = findViewById(R.id.btn_send);
         text_send = findViewById(R.id.text_send);
         recyclerView = findViewById(R.id.recycler_view);
         // 設定recyclerView
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MessageAdapter(this,mchat));
+        messageAdapter = new MessageAdapter(this, mchat);
+        recyclerView.setAdapter(messageAdapter);
 
-        // intent=getIntent();
-        // String message = intent.getStringExtra("id");
+        btn_back.setOnClickListener(this);
+        btn_send.setOnClickListener(this);
+        intent = getIntent();
+    }
 
-        btn_send.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                // 存取字串
-                String msg = text_send.getText().toString();
-                // 驗證訊息是否空白
-                if(!msg.equals("")){
-                    // 用資料結構打包資料(user sent)
-                    mchat.add(new Chat(VIEW_TYPE_MESSAGE_SENT,msg));
-                    // 傳到後端連線，傳mChat物件到後端
-                    sendMessage(msg);
-                }else{
-                    Toast.makeText(MessageActivity.this,"訊息不能空白",Toast.LENGTH_SHORT).show();
+    @Override
+    public void onClick(View v) {
+        if (v == btn_send) {
+            // 存取字串
+            String msg = text_send.getText().toString();
+            // 驗證訊息是否空白
+            if (!msg.equals("")) {
+                // 用資料結構打包資料(user sent) update adapter
+                mchat.add(new Chat(VIEW_TYPE_MESSAGE_SENT, msg));
+
+                // 新增執行緒(線程) Socket連線
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try (Socket s = new Socket(ip, port)) {
+                            // setting socket write & read
+                            System.out.println("Just connected to " + s.getRemoteSocketAddress());
+                            s.setSoTimeout(60 * 1000);
+                            // 傳到server
+                            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+                            // server回傳
+                            InputStream dataIn = s.getInputStream();
+                            byte[] buffer = new byte[1024];
+
+                            // send data to server
+                            out.write(mchat.get(mchat.size() - 1).getMessage().getBytes());
+                            out.flush();
+
+                            // receive data
+                            msg_recv = new String(buffer, 0, dataIn.read(buffer));
+                            // debug_print
+                            System.out.print(msg_recv);
+                            System.out.flush();
+
+                            // signal main thread
+                            synchronized (syncObj) {
+                                System.out.println("notify main thread");
+                                syncObj.notify();
+                            }
+                            // close write & read & socket
+                            System.out.println("Close InputStream");
+                            out.close();
+                            dataIn.close();
+                            if (!s.isClosed()) {
+                                s.close();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+                // block main thread until recieved data to msg_recv
+                synchronized (syncObj) {
+                    try {
+                        syncObj.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                // 訊息傳完後，訊息格要清除文字
-                text_send.setText("");
+                // update adapter
+                mchat.add(new Chat(VIEW_TYPE_MESSAGE_RECEIVED, msg_recv));
+                // refresh recycleview
+                messageAdapter.notifyDataSetChanged();
+                // debug (main thread signaled)
+                Toast.makeText(this, "send success", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MessageActivity.this, "訊息不能空白", Toast.LENGTH_SHORT).show();
             }
-        });
-    }
-
-    // 連線到後端
-    private void sendMessage(String message){
-        // Socket 寫在這下面(chat room->python)
-
-    }
-
-    // 從後端回傳
-    private void readMessage(String myid,String userid){
-        // 靠左的訊息
-        mchat = new ArrayList<>();
-        // Socket 寫在這下面(python-> chat room)
-
-
+            // 訊息傳完後，訊息格清除文字
+            text_send.getText().clear();
+        } else if (v == btn_back) {
+            // 回到首頁
+            finish();
+        }
     }
 }
---
+++ predict.py
@@ -0,0 +1,129 @@
+# coding: utf-8
+
+from __future__ import print_function
+
+import os
+import tensorflow as tf
+import tensorflow.contrib.keras as kr
+
+from cnn_model import TCNNConfig, TextCNN
+from rnn_model import TRNNConfig, TextRNN
+from data.cnews_loader import read_category, read_vocab
+# -----------------------------------------------------
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import xlrd
+import socket
+from _thread import start_new_thread
+# -----------------------------------------------------
+try:
+    bool (type (unicode))
+except NameError:
+    unicode = str
+# data path(model,Q&A database)
+base_dir = 'data/cnews'
+vocab_dir = os.path.join (base_dir, 'cnews.vocab.txt')
+
+save_dir = 'checkpoints/textrnn'
+save_path = os.path.join (save_dir, 'best_validation')  # 最佳验证结果保存路径
+
+
+# keep
+class CnnModel:
+    def __init__(self):
+        self.config = TRNNConfig ()  # <--TCNNConfig
+        self.categories, self.cat_to_id = read_category ()
+        self.words, self.word_to_id = read_vocab (vocab_dir)
+        self.config.vocab_size = len (self.words)
+        self.model = TextRNN (self.config)  # <--TextCNN
+
+        self.session = tf.Session ()
+        self.session.run (tf.global_variables_initializer ())
+        saver = tf.train.Saver ()
+        saver.restore (sess=self.session, save_path=save_path)  # 读取保存的模型
+
+    def predict(self, message):
+        # 支持不论在python2还是python3下训练的模型都可以在2或者3的环境下运行
+        content = unicode (message)
+        data = [self.word_to_id[x] for x in content if x in self.word_to_id]
+
+        feed_dict = {
+            self.model.input_x: kr.preprocessing.sequence.pad_sequences ([data], self.config.seq_length),
+            self.model.keep_prob: 1.0
+        }
+
+        y_pred_cls = self.session.run (self.model.y_pred_cls, feed_dict=feed_dict)
+        return self.categories[y_pred_cls[0]]
+
+
+# type(Q) == str
+def demo_test(Question):
+    # ---get message---start replying
+    print (f"the question is {Question}。")
+    # 分類問題_debug
+    print (f"the question category belongs to {cnn_model.predict (Question)}.")
+    # 分類問題類別(10類)，開啟該工作表
+    ws = wb.sheet_by_name (cnn_model.predict (Question))
+    # init and reset & reuse dictionary
+    f = {}
+    # iterate rows from A1 to B2 in dictionary f
+    for j in range (ws.nrows):
+        # key:question,val:answer
+        f[ws.cell (j, 0).value] = ws.cell (j, 1).value
+    # 取得最相似的問題
+    answer = process.extractOne (Question, [questions for questions in f])
+    # (最相似的問題,相似度)_debug
+    print (answer)
+
+    # 二次比對問題相似度_debug
+    print (f"fuzz.token_set_ratio:{fuzz.token_set_ratio (answer[0], Question)}")
+
+    # 依照最佳回應程度分別做回應
+    if answer[1] > 60 and fuzz.token_set_ratio (answer[0], Question) > 60:
+        print (f"The best reply is {f.get (answer[0])}.")
+        return "The best reply is " + f.get (answer[0])
+    elif answer[1] > 50:
+        print (f"The answer might be {f.get (answer[0])}.")
+        return "The answer might be " + f.get (answer[0])
+    else:
+        print ("sorry I don't get it")
+        return "sorry I don't get it"
+
+
+# single thread socket connection
+def clientthread(connection):
+    while True:
+        connection.settimeout (None)
+        # type(data)==bytes
+        data = connection.recv (1024)
+        while len (data) is 0:
+            pass
+
+        # response to client
+        connection.send (bytes (demo_test (data.decode ('UTF-8')), encoding='utf-8'))
+
+    connection.close ()
+    print ("server closed")
+
+
+if __name__ == '__main__':
+    # loading...
+    # open .xml file
+    wb = xlrd.open_workbook ('xml.xlsx')
+    # loading model
+    cnn_model = CnnModel ()
+    # ---loading complete---
+    HOST = "163.13.28.46"  # see shell>ipconfig
+    PORT = 8000
+    print (HOST)
+    # starting up socket ...
+    with socket.socket (socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind ((HOST, PORT))
+        s.listen ()
+        while 1:
+            # ---linking complete---wait for client connection...
+            print ("listen for client...")
+            connect, addr = s.accept ()
+            print ("get client")
+            # create new thread for every single client
+            start_new_thread (clientthread, (connect,))
